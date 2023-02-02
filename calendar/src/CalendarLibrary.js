4f74589aeec876a4606989d3287fa1f68b54cba7
Julian Sammer <julian.sammer@student.htldornbirn.at>
2023-02-02 10:21:13 +0100

    calender

---
+++ calendar/src/CalendarLibrary.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './App.css';
+
+function CalendarLibrary() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className='app'>
+      <h1 className='text-center'>Kalender mit React</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Ausgewähltes Datum:</span>{' '}
+        {date.toDateString()}
+      </p>
+    </div>
+  );
+}
+
+export default CalendarLibrary;

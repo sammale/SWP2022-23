f357ba25efc048f2ee7adef1aa9d71cb282a0640
Julian Sammer <julian.sammer@student.htldornbirn.at>
2023-02-02 11:29:46 +0100

    calender css

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-import CalendarLibrary from './CalendarLibrary.js';
+import CalendarLibrary from './components/CalendarLibrary.js';
 import './App.css';
-
+import './components/Calendar.css'
 function App() {
   return (
     <div className="App">
